043821dc62a9fe8d1167bd6025be0188467162d4
Édio Melo <e.mello.dev@gmail.com>
2024-05-08 17:59:47 -0400

    Fixed some eslint and prettier errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,13 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': ['error', { singleQuote: false }],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+        endOfLine: 'lf',
+      },
+    ],
   },
 };
